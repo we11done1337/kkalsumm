fe07f151e19d20aa659adc4f8a11451d8ebcc97b
Валера <kasatkin.1995@list.ru>
2023-01-04 14:44:21 +0300

    practise

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
-tickets = int(input("Введите необходимое вам количество билетов: "))
-visitor = tickets
-
-cost = 0
-visitor = 1
-while visitor <= tickets:
-    age_of_visitor = int(input(f'Укажите для какого возраста приобретается билет № {visitor} ? '))
-    if age_of_visitor < 18:
-        print('Билет будет бесплатный')
-    elif 25 > age_of_visitor >= 18:
-        cost += 990
-        print('Стоимость билета будет 990 руб.')
-    else:
-        cost += 1390
-        print('Стоимость билета будет 1390 руб.')
-    visitor += 1
-
-if tickets > 3:
-    sale = cost - (cost * 0.1)
-    print(f'Итого к оплате {sale} руб., с применением 10%-ой скидки за покупку более 3 билетов')
+sex = input("Введите ваш пол: ")
+weigth = int(input("Введите свой вес в кг: "))
+heigth = int(input("Введите свой рост в см: "))
+age = int(input("Введите свой возраст: "))
+if sex == "женский":
+    result_w = round((weigth*10) + (6.25*heigth) + (age*5) - 161)
+    print("Ваша норма ", result_w, "ккал")
 else:
-    print(f'Итого к оплате {cost} руб.')
-visitor += 1
+    result_m = round((weigth*10) + (6.25*heigth) + (age*5) + 5)
+    print("Ваша норма ", result_m, "ккал")
+
+
